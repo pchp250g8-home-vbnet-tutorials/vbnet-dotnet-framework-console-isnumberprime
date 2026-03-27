745361292562473e8a799b8c7c63ed27411c064d
Alexander <pchp250g8@tutamail.com>
2026-03-27 13:26:38 +0300

    Add project files.

---
+++ IsNimberPrime/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ulNumber As ULong
+        Console.WriteLine("Input an integer positive number")
+        ULong.TryParse(Console.ReadLine(), ulNumber)
+        Console.WriteLine($"The number {ulNumber} has the folowing dividers:")
+        Dim nDividers = 0
+        For i As ULong = 1 To ulNumber
+            If (ulNumber Mod i = 0) Then
+                Console.Write("{0} ", i)
+                nDividers += 1
+            End If
+        Next
+        Console.WriteLine()
+        Select Case nDividers
+            Case 0, 1
+                Console.WriteLine($"The number {ulNumber} is neither composite nor prime")
+            Case 2
+                Console.WriteLine($"The number {ulNumber} is prime")
+            Case Else
+                Console.WriteLine($"The number {ulNumber} is composite")
+        End Select
+        Console.Read()
+    End Sub
+
+End Module
---
+++ IsNimberPrime/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IsNimberPrime")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("IsNimberPrime")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0c9f9ea5-1134-42a0-b732-8c7556137484")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
